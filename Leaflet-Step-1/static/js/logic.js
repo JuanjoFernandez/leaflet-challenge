95caffacf2c9d8ad844cb2ffe7eeefee1df3ccd0
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-07-14 23:16:04 -0500

    Changes map style to dark

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,57 +2,54 @@
 // Functions
 // *********
 
-// CreateFeatures
-function createFeatures(earthquakeData) {
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+// // CreateFeatures
+// function createFeatures(earthquakeData) {
+//   function onEachFeature(feature, layer) {
+//     layer.bindPopup("<h3>" + feature.properties.place +
+//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+//   }
+
+//   function createMap(earthquakes) {
+
+//     // Define streetmap and darkmap layers
+//     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//       tileSize: 512,
+//       maxZoom: 18,
+//       zoomOffset: -1,
+//       id: "mapbox/streets-v11",
+//       accessToken: API_KEY
+//     });
+//   }
 
 
+  // Creating the map
+  var myMap = L.map("map", {
+    center: [40, -95],
+    zoom: 5
+  });
 
-// Creating the map
-var myMap = L.map("map", {
-  center: [45.52, -122.67],
-  zoom: 2.5
-});
-
-// Adding the background map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-// Request JSON
-var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-d3.json(queryURL).then(function (data) {
-  createFeatures(data.features);
-});
-
-// Create a GeoJSON layer containing the features array on the earthquakeData object
-// Run the onEachFeature function once for each piece of data in the array
-var earthquakes = L.geoJSON(earthquakeData, {
-  onEachFeature: onEachFeature
-});
-
-// Sending our earthquakes layer to the createMap function
-createMap(earthquakes);
-
-
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // Adding the background map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "mapbox/dark-v10",
     accessToken: API_KEY
-  });
+  }).addTo(myMap);
 
-}
+  // // Request JSON
+  // var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  // d3.json(queryURL).then(function (data) {
+  //   createFeatures(data.features);
+  // });
+
+  // // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // // Run the onEachFeature function once for each piece of data in the array
+  // var earthquakes = L.geoJSON(earthquakeData, {
+  //   onEachFeature: onEachFeature
+  // });
+
+  // // Sending our earthquakes layer to the createMap function
+  // createMap(earthquakes);
